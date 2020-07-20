676b9bdbc3bbe5af250c1fb7954bb91029365ee4
pranjali24 <pranjuk464@gmail.com>
2020-07-20 12:49:48 +0530

    check anwer correction when click on option

--- test.js
+++ test.js
@@ -37,7 +37,7 @@ function displayQuestion() {
 // display options 
 function displayOptions(que) {
   return que.options.map(option => {
-    return `<li class="test__option"><input type="radio" name="option"> ${option}</li>`;
+    return `<li class="test__option"><input type="radio" name="option">${option}</li>`;
   });
 }
 
@@ -65,12 +65,32 @@ function matchAnswer(){
         }, 10000);
     }
   });
-
+// display answer when clicked button
   function showAnswer(answer){
     answer.style.display = 'block'
  }
+//  hide answer after show answer
  function hideAnswer(answer)  {
    answer.style.display='none';
  }
 }
 
+/* Check answer when click radio button */
+const options=document.querySelectorAll('.test__option');
+options.forEach(option=>option.addEventListener('click',optionClick));
+function optionClick(){
+  let selectOption=this.innerText;
+  
+  var check= questionArray.some(que=>{
+      if(que.answer===selectOption){
+        return true;
+      }  
+  });
+  // Add symbol right or wrong
+  if(check===true){
+    this.insertAdjacentHTML('beforeend', '<span class="option__check">&check;</span>')
+  }
+  else{
+    this.insertAdjacentHTML('beforeend', '<span class="option__check option__check--red">&cross;</span>')
+  }
+}
